a01606c891f2eba5a42c340ea397650997e3fa51
David White <dpwhite2@ncsu.edu>
2010-10-13 10:25:17 -0400

    Added some docstrings to htmltotext.py.

--- linkanalytics/util/htmltotext.py
+++ linkanalytics/util/htmltotext.py
@@ -25,6 +25,7 @@ class HTMLtoText(HTMLParser):
         return self.buf
     
     def _init_handlers(self):
+        """Initializes dictionary of element-specific handler functions."""
         self.handlers = {
             'p': (self.p_start, self.p_end),
             'br': (self.br_tag, self._donothing_endtag),
@@ -38,32 +39,45 @@ class HTMLtoText(HTMLParser):
             }
             
     def _donothing_starttag(self, attrs):
+        """Default element handler function that does nothing."""
         pass
     def _donothing_endtag(self):
+        """Default element handler function that does nothing."""
         pass
         
     def br_tag(self, attrs):
+        """Handle a <br/> tag."""
         self.buf += '\n'
         
     def p_start(self, attrs):
+        """Handle an opening paragraph tag."""
         self.buf += '\n'
     def p_end(self):
+        """Handle a closing paragraph tag."""
         self.buf += '\n'
         
     def heading_start(self, attrs, level):
+        """Handle an opening heading tag."""
         self.buf += '\n'
     def heading_end(self, level):
+        """Handle a closing heading tag."""
         self.buf += '\n'
         
     def body_start(self, attrs):
+        """Handle an opening body tag."""
         self.inbody = True
     def body_end(self):
+        """Handle a closing body tag."""
         self.inbody = False
     
     def handle_starttag(self, tag, attrs):
+        """Handle a start tag by dispatching to the appropriate 
+           element-specific function."""
         self.handlers.get(tag, (self._donothing_starttag,))[0](attrs)
 
     def handle_endtag(self, tag):
+        """Handle a end tag by dispatching to the appropriate 
+           element-specific function."""
         self.handlers.get(tag, (None, self._donothing_endtag))[1]()
         
     def handle_entityref(self, name):
@@ -73,11 +87,11 @@ class HTMLtoText(HTMLParser):
         
     def handle_charref(self, name):
         if name.startswith('x') or name.startswith('X'):
-            cp = int(name[1:],16)
+            cp = int(name[1:], 16)
         else:
             cp = int(name)
         u = unichr(cp)
-        self.buf += codecs.iterencode(u,'utf-8')
+        self.buf += codecs.iterencode(u, 'utf-8')
         
     def handle_data(self, data):
         if (not self.full_document) or self.inbody:
